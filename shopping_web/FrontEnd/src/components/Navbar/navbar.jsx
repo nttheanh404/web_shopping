c385ee148d1e650c9ea2a247afddc9260f871659
avpegnu <vietanh142004@gmail.com>
2025-03-19 00:18:47 +0700

    collection, offer, footer

--- shopping_web/FrontEnd/src/components/Navbar/navbar.jsx
+++ shopping_web/FrontEnd/src/components/Navbar/navbar.jsx
@@ -1,11 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 import "./navbar.css";
 import logo from "../assets/logo.png";
 import cart_icon from "../assets/cart_icon.png";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  const [menu, setMenu] = useState("shop");
+  // const [menu, setMenu] = useState("shop");
 
   return (
     <div className="navbar">
@@ -26,37 +26,59 @@ const Navbar = () => {
       </div>
 
       <ul className="nav-menu">
-        <li onClick={() => setMenu("shop")}>
-          <Link style={{ textDecoration: "none" }} to="/">
+        {/* <li onClick={() => setMenu("shop")}> */}
+        <li>
+          <Link
+            style={{ textDecoration: "none", color: "currentColor" }}
+            to="/"
+          >
             Shop
           </Link>
-          {menu === "shop" ? <hr /> : null}
+          {/* {menu === "shop" ? <hr /> : null} */}
         </li>
-        <li onClick={() => setMenu("mens")}>
-          <Link style={{ textDecoration: "none" }} to="/mens">
+        {/* <li onClick={() => setMenu("mens")}> */}
+        <li>
+          <Link
+            style={{ textDecoration: "none", color: "currentColor" }}
+            to="/mens"
+          >
             Men
           </Link>
-          {menu === "mens" ? <hr /> : null}
+          {/* {menu === "mens" ? <hr /> : null} */}
         </li>
-        <li onClick={() => setMenu("womens")}>
-          <Link style={{ textDecoration: "none" }} to="/womens">
+        {/* <li onClick={() => setMenu("womens")}> */}
+        <li>
+          <Link
+            style={{ textDecoration: "none", color: "currentColor" }}
+            to="/womens"
+          >
             Women
           </Link>
-          {menu === "womens" ? <hr /> : null}
+          {/* {menu === "womens" ? <hr /> : null} */}
         </li>
-        <li onClick={() => setMenu("kids")}>
-          <Link style={{ textDecoration: "none" }} to="/kids">
+        {/* <li onClick={() => setMenu("kids")}> */}
+        <li>
+          <Link
+            style={{ textDecoration: "none", color: "currentColor" }}
+            to="/kids"
+          >
             Kids
           </Link>
-          {menu === "kids" ? <hr /> : null}
+          {/* {menu === "kids" ? <hr /> : null} */}
         </li>
       </ul>
 
       <div className="nav-login-cart">
-        <Link style={{ textDecoration: "none", color: "currentColor" }} to="/login">
+        <Link
+          style={{ textDecoration: "none", color: "currentColor" }}
+          to="/login"
+        >
           <button>Log in</button>
         </Link>
-        <Link style={{ textDecoration: "none", color: "currentColor" }} to="/cart">
+        <Link
+          style={{ textDecoration: "none", color: "currentColor" }}
+          to="/cart"
+        >
           <img src={cart_icon} alt="cart icon" />
         </Link>
         <div className="nav-cart-count">0</div>
