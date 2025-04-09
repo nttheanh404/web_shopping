0aa7ad823999401cf4e2a17e37f6631b2b014e27
Hoang Ngoc Anh <hngocanh208203@gmail.com>
2025-04-09 23:12:04 +0700

    11:11pm-09/04/2025

--- Admin/src/App.jsx
+++ Admin/src/App.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import Navbar from './Components/Navbar/Navbar'
+import Admin from './Pages/Admin/Admin'
+
 const App=()=>{
   return(
     <div>
-      {/* <p>fdhfuadshfhjadfadhjs</p> */}
       <Navbar/>
       <Admin/>
     </div>
--- Admin/src/Components/Sidebar/Sidebar.jsx
+++ Admin/src/Components/Sidebar/Sidebar.jsx
@@ -1,19 +1,21 @@
 import React from 'react'
 import './Sidebar.css'
-import { Link } from 'react-router-dom'
+import {Link} from 'react-router-dom'
 import add_product_icon from '../../assets/Product_cart.svg'
 import list_product_icon from '../../assets/Product_list_icon.svg'
 const Sidebar=()=>{
   return(
     <div className='sidebar'>
         <Link to={'/addproduct'} style={{textDecoration:"none"}}>
-            <div>
+        <div className="sidebar-item">
+
                 <img src={add_product_icon} alt=""/>
                 <p>Add Product</p>
             </div>
         </Link>
         <Link to={'/listproduct'} style={{textDecoration:"none"}}>
-            <div>
+            <div className="sidebar-item">
+            
                 <img src={list_product_icon} alt=""/>
                 <p>Product List</p>
             </div>
@@ -21,4 +23,4 @@ const Sidebar=()=>{
     </div>
   )
 }
-export default Admin
+export default Sidebar
