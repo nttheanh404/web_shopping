87949cffc77c30c8f8e947c5debd808bd8ca3752
avpegnu <vietanh142004@gmail.com>
2025-04-17 15:02:35 +0700

    orderManagement

--- shopping_web/Admin/src/Components/Sidebar/Sidebar.jsx
+++ shopping_web/Admin/src/Components/Sidebar/Sidebar.jsx
@@ -1,26 +1,34 @@
-import React from 'react'
-import './Sidebar.css'
-import {Link} from 'react-router-dom'
-import add_product_icon from '../../assets/Product_Cart.svg'
-import list_product_icon from '../../assets/Product_list_icon.svg'
+import React from "react";
+import "./Sidebar.css";
+import { Link } from "react-router-dom";
+import add_product_icon from "../../assets/Product_Cart.svg";
+import list_product_icon from "../../assets/Product_list_icon.svg";
 
 const Sidebar = () => {
   return (
-    <div className='sidebar'>
-        <Link to={'/addproduct'} style={{textDecoration:"none"}}>
-          <div className="sidebar-item">
-            <img src={add_product_icon} alt="" />
-            <p>Add Product</p>
-          </div>
-        </Link>
-        <Link to={'/listproduct'} style={{textDecoration:"none"}}>
-          <div className="sidebar-item">
-            <img src={list_product_icon} alt="" />
-            <p>Product List</p>
-          </div>
-        </Link>
+    <div className="sidebar">
+      <Link to={"/addproduct"} style={{ textDecoration: "none" }}>
+        <div className="sidebar-item" style={{ paddingLeft: "20px" }}>
+          <img src={add_product_icon} alt="" />
+          <p>Add Product</p>
+        </div>
+      </Link>
+      <Link to={"/listproduct"} style={{ textDecoration: "none" }}>
+        <div className="sidebar-item" style={{ paddingLeft: "20px" }}>
+          <img src={list_product_icon} alt="" />
+          <p>Product List</p>
+        </div>
+      </Link>
+      <Link to={"/ordermanagement"} style={{ textDecoration: "none" }}>
+        <div className="sidebar-item">
+          {/* <img src={order_management_icon} alt="" /> */}
+          <p className="sidebar-item-text">
+            <span className="icon-management">🧾 </span> Order Management
+          </p>
+        </div>
+      </Link>
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
--- shopping_web/Admin/src/Pages/Admin/Admin.jsx
+++ shopping_web/Admin/src/Pages/Admin/Admin.jsx
@@ -1,20 +1,22 @@
-import React from 'react'
-import './Admin.css'
-import Sidebar from '../../Components/Sidebar/Sidebar'
-import {Routes, Route} from 'react-router-dom'
-import AddProduct from '../../Components/AddProduct/AddProduct'
-import ListProduct from '../../Components/ListProduct/ListProduct'
+import React from "react";
+import "./Admin.css";
+import Sidebar from "../../Components/Sidebar/Sidebar";
+import { Routes, Route } from "react-router-dom";
+import AddProduct from "../../Components/AddProduct/AddProduct";
+import ListProduct from "../../Components/ListProduct/ListProduct";
+// import OrderManagement from "../../Components/OrderManagement/OrderManagement";
 
 const Admin = () => {
   return (
-    <div className='admin'>
-        <Sidebar/>
-        <Routes>
-            <Route path='/addproduct' element={<AddProduct/>}/>
-            <Route path='/listproduct' element={<ListProduct/>}/>
-        </Routes>
+    <div className="admin">
+      <Sidebar />
+      <Routes>
+        <Route path="/addproduct" element={<AddProduct />} />
+        <Route path="/listproduct" element={<ListProduct />} />
+        {/* <Route path="/ordermanagement" element={<OrderManagement />} /> */}
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default Admin
+export default Admin;
